819c5485e46a755d0ea602cde4f18302572b9d9f
AhmedHossam06 <ahmedhossameldin1@icloud.com>
2026-05-14 02:48:05 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,18 +4,77 @@
  */
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+       
         int[] A = {1, 3, 6, 4, 1, 2};
-        int[] B = {1,2,3};
-        int[] C = {-1,-2};
-        System.out.println("Non Recursive: " + MissingIntegerNonRecursive.findMissing(A));
-        System.out.println("Recursive: " + MissingIntegerRecursive.findMissing(A));
-        System.out.println("Non Recursive: " + MissingIntegerNonRecursive.findMissing(B));
-        System.out.println("Recursive: " + MissingIntegerRecursive.findMissing(B));
-        System.out.println("Non Recursive: " + MissingIntegerNonRecursive.findMissing(C));
-        System.out.println("Recursive: " + MissingIntegerRecursive.findMissing(C));
+        int[] B = {1, 2, 3};
+        int[] C = {-1, -2};
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int N = sc.nextInt();
+
+        
+        if (N < 1 || N > 100000) {
+            System.out.println("Array size must be between 1 and 100000");
+            return;
+        }
+
+        int[] d = new int[N];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < N; i++) {
+
+            d[i] = sc.nextInt();
+
+            
+            if (d[i] < -1000000 || d[i] > 1000000) {
+                System.out.println(
+                    "Elements must be between -1000000 and 1000000"
+                );
+                return;
+            }
+        }
+
+        System.out.println("User Array:");
+        System.out.println(
+            "Non Recursive: " +
+            MissingIntegerNonRecursive.findMissing(d)
+        );
+
+        System.out.println(
+            "Recursive: " +
+            MissingIntegerRecursive.findMissing(d)
+        );
+    
+
+        
+        System.out.println("\nArray A:");
+        System.out.println("Non Recursive: " +
+                MissingIntegerNonRecursive.findMissing(A));
+
+        System.out.println("Recursive: " +
+                MissingIntegerRecursive.findMissing(A));
+
+        System.out.println("\nArray B:");
+        System.out.println("Non Recursive: " +
+                MissingIntegerNonRecursive.findMissing(B));
+
+        System.out.println("Recursive: " +
+                MissingIntegerRecursive.findMissing(B));
+
+        System.out.println("\nArray C:");
+        System.out.println("Non Recursive: " +
+                MissingIntegerNonRecursive.findMissing(C));
+
+        System.out.println("Recursive: " +
+                MissingIntegerRecursive.findMissing(C));
     }
 }
